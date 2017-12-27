e4f8d0dd69c3566f51d5ad0fa2f620d9151643d2
Rafka <fw.ge-mu.930@jb4.so-net.ne.jp>
2017-12-28 02:18:02 +0900

    Add gbequ functions.

--- SharpMKL/LAPACK/generalBand.cs
+++ SharpMKL/LAPACK/generalBand.cs
@@ -26,5 +26,24 @@ namespace SharpMKLStd {
     [DllImport(LibPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "LAPACKE_dgbtrs")]
     public static extern int gbtrs(LapackLayout Layout, LapackTranspose Trans, int n, int kl, int ku, int nrhs,
                                    double[] ab, int ldab, int[] ipiv, double[] b, int ldb);
+
+    [DllImport(LibPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "LAPACKE_sgbequ")]
+    public static extern int gbequ(LapackLayout Layout, int m, int n, int kl, int ku, float[] ab, int ldab,
+                                   float[] r, float[] c, out float rowCnd, out float colCnd, out float aMax);
+    public static int gbequ(LapackLayout Layout, int m, int n, int kl, int ku, float[] ab, int ldab,
+                            out float[] r, out float[] c, out float rowCnd, out float colCnd, out float aMax) {
+      r = new float[m];
+      c = new float[n];
+      return gbequ(Layout, m, n, kl, ku, ab, ldab, r, c, out rowCnd, out colCnd, out aMax);
+    }
+    [DllImport(LibPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "LAPACKE_dgbequ")]
+    public static extern int gbequ(LapackLayout Layout, int m, int n, int kl, int ku, double[] ab, int ldab,
+                                   double[] r, double[] c, out double rowCnd, out double colCnd, out double aMax);
+    public static int gbequ(LapackLayout Layout, int m, int n, int kl, int ku, double[] ab, int ldab,
+                            out double[] r, out double[] c, out double rowCnd, out double colCnd, out double aMax) {
+      r = new double[m];
+      c = new double[n];
+      return gbequ(Layout, m, n, kl, ku, ab, ldab, r, c, out rowCnd, out colCnd, out aMax);
+    }
   }
 }
