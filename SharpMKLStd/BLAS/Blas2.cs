1a57e5faedc567d067f53673f7396ae14f77a25a
Rafka <fw.ge-mu.930@jb4.so-net.ne.jp>
2017-12-27 22:25:40 +0900

    Add ger functions.

--- SharpMKLStd/BLAS/Blas2.cs
+++ SharpMKLStd/BLAS/Blas2.cs
@@ -21,5 +21,14 @@ namespace SharpMKLStd {
     public static extern void gemv(BlasLayout Layout, BlasTranspose Trans,
                                    int m, int n, double alpha, double[] a, int lda,
                                    double[] x, int incX, double beta, double[] y, int incY);
+
+    [DllImport(LibPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "cblas_sger")]
+    public static extern void ger(BlasLayout Layout, int m, int n,
+                                  float alpha, float[] x, int incX, float[] y, int incY,
+                                  float[] a, int lda);
+    [DllImport(LibPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "cblas_dger")]
+    public static extern void ger(BlasLayout Layout, int m, int n,
+                                  double alpha, double[] x, int incX, double[] y, int incY,
+                                  double[] a, int lda);
   }
 }
