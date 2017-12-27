8d391e0fbdf8c622983dd7ffae71542129448e36
Rafka <fw.ge-mu.930@jb4.so-net.ne.jp>
2017-12-27 19:25:31 +0900

    Add geequb functions.

--- SharpMKLStd/LAPACK/General.cs
+++ SharpMKLStd/LAPACK/General.cs
@@ -54,5 +54,33 @@ namespace SharpMKLStd {
       c = new double[n];
       return geequ(Layout, m, n, a, lda, r, c, out rowCnd, out colCnd, out aMax);
     }
+
+    [DllImport(LibPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "LAPACKE_sgeequb")]
+    public static extern int geequb(LapackLayout Layout,
+                                    int m, int n, float[] a, int lda,
+                                    float[] r, float[] c,
+                                    out float rowCnd, out float colCnd, out float aMax);
+    public static int geequb(LapackLayout Layout,
+                             int m, int n, float[] a, int lda,
+                             out float[] r, out float[] c,
+                             out float rowCnd, out float colCnd, out float aMax) {
+      r = new float[m];
+      c = new float[n];
+      return geequb(Layout, m, n, a, lda, r, c, out rowCnd, out colCnd, out aMax);
+    }
+
+    [DllImport(LibPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "LAPACKE_dgeequb")]
+    public static extern int geequb(LapackLayout Layout,
+                                    int m, int n, double[] a, int lda,
+                                    double[] r, double[] c,
+                                    out double rowCnd, out double colCnd, out double aMax);
+    public static int geequb(LapackLayout Layout,
+                             int m, int n, double[] a, int lda,
+                             out double[] r, out double[] c,
+                             out double rowCnd, out double colCnd, out double aMax) {
+      r = new double[m];
+      c = new double[n];
+      return geequb(Layout, m, n, a, lda, r, c, out rowCnd, out colCnd, out aMax);
+    }
   }
 }
