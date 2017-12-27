f6054a4db6e5165ea4b2cae1d13572441016693c
Rafka <fw.ge-mu.930@jb4.so-net.ne.jp>
2017-12-28 00:00:58 +0900

    Add gemm functions.

--- SharpMKL/BLAS/Blas3.cs
+++ SharpMKL/BLAS/Blas3.cs
@@ -1,5 +1,14 @@
-﻿namespace SharpMKLStd {
+﻿using System.Runtime.InteropServices;
+
+namespace SharpMKLStd {
   public static class Blas3 {
     private const string LibPath = "mkl_rt.dll";
+
+    [DllImport(LibPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "cblas_sgemm")]
+    public static extern void gemm(BlasLayout Layout, BlasTranspose TransA, BlasTranspose TransB, int m, int n, int k,
+                                   float alpha, float[] a, int lda, float[] b, int ldb, float beta, float[] c, int ldc);
+    [DllImport(LibPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "cblas_dgemm")]
+    public static extern void gemm(BlasLayout Layout, BlasTranspose TransA, BlasTranspose TransB, int m, int n, int k,
+                                   double alpha, double[] a, int lda, double[] b, int ldb, double beta, double[] c, int ldc);
   }
 }
