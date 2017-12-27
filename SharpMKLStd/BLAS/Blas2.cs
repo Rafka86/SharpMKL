55bbd59c79098da9590842fd6b71702c102e67fd
Rafka <fw.ge-mu.930@jb4.so-net.ne.jp>
2017-12-27 21:57:30 +0900

    Add gbmv functions.

---
+++ SharpMKLStd/BLAS/Blas2.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+
+namespace SharpMKLStd {
+  public static class Blas2 {
+    private const string LibPath = "mkl_rt.dll";
+
+    [DllImport(LibPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "cblas_sgbmv")]
+    public static extern void gbmv(BlasLayout Layout, BlasTranspose Trans,
+                                   int m, int n, int kl, int ku, float alpha, float[] a, int lda,
+                                   float[] x, int incX, float beta, float[] y, int incY);
+    [DllImport(LibPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "cblas_dgbmv")]
+    public static extern void gbmv(BlasLayout Layout, BlasTranspose Trans,
+                                   int m, int n, int kl, int ku, double alpha, double[] a, int lda,
+                                   double[] x, int incX, double beta, double[] y, int incY);
+  }
+}
---
+++ SharpMKLStd/BLAS/BlasEnum.cs
@@ -0,0 +1,12 @@
+﻿namespace SharpMKLStd {
+  public enum BlasLayout {
+    RowMajor = 101,
+    ColumnMajor = 102
+  }
+
+  public enum BlasTranspose {
+    NoTrans = 111,
+    Trans = 112,
+    ConjTrans = 113
+  }
+}
