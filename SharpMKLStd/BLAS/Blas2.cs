56a4ed9cb4a4c71a683d55143c3802661962b429
Rafka <fw.ge-mu.930@jb4.so-net.ne.jp>
2017-12-27 22:52:06 +0900

    Add spr functions.

--- SharpMKLStd/BLAS/Blas2.cs
+++ SharpMKLStd/BLAS/Blas2.cs
@@ -30,19 +30,26 @@ namespace SharpMKLStd {
     public static extern void ger(BlasLayout Layout, int m, int n,
                                   double alpha, double[] x, int incX, double[] y, int incY,
                                   double[] a, int lda);
-                                 
+
     [DllImport(LibPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "cblas_ssbmv")]
     public static extern void sbmv(BlasLayout Layout, BlasUpLo UpLo, int n, int k, float alpha, float[] a, int lda,
                                    float[] x, int incX, float beta, float[] y, int incY);
     [DllImport(LibPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "cblas_dsbmv")]
     public static extern void sbmv(BlasLayout Layout, BlasUpLo UpLo, int n, int k, double alpha, double[] a, int lda,
                                    double[] x, int incX, double beta, double[] y, int incY);
-                                 
+
     [DllImport(LibPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "cblas_sspmv")]
     public static extern void spmv(BlasLayout Layout, BlasUpLo UpLo, int n, float alpha, float[] ap,
                                    float[] x, int incX, float beta, float[] y, int incY);
     [DllImport(LibPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "cblas_dspmv")]
     public static extern void spmv(BlasLayout Layout, BlasUpLo UpLo, int n, double alpha, double[] ap,
                                    double[] x, int incX, double beta, double[] y, int incY);
+
+    [DllImport(LibPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "cblas_sspr")]
+    public static extern void spr(BlasLayout Layout, BlasUpLo UpLo,
+                                  int n, float alpha, float[] x, int incX, float[] ap);
+    [DllImport(LibPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "cblas_dspr")]
+    public static extern void spr(BlasLayout Layout, BlasUpLo UpLo, int n,
+                                  double alpha, double[] x, int incX, double[] ap);
   }
 }
