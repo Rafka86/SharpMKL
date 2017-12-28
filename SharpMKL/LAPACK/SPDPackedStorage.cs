e2f32f420a1f900db963f9d9d3c39fa71d3d5c1b
Rafka <fw.ge-mu.930@jb4.so-net.ne.jp>
2017-12-29 00:07:13 +0900

    Add ppequ functions.

--- SharpMKL/LAPACK/SPDPackedStorage.cs
+++ SharpMKL/LAPACK/SPDPackedStorage.cs
@@ -16,5 +16,21 @@ namespace SharpMKLStd {
     public static extern int pptrs(LapackLayout Layout, LapackUpLo UpLo,
                                    int n, int nrhs, double[] ap, double[] b, int ldb);
     
+    [DllImport(LibPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "LAPACKE_sppequ")]
+    public static extern int ppequ(LapackLayout Layout, LapackUpLo UpLo,
+                                   int n, float[] ap, float[] s, out float sCond, out float aMax);
+    public static int ppequ(LapackLayout Layout, LapackUpLo UpLo,
+                            int n, float[] ap, out float[] s, out float sCond, out float aMax) {
+      s = new float[n];
+      return ppequ(Layout, UpLo, n, ap, s, out sCond, out aMax);
+    }
+    [DllImport(LibPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "LAPACKE_dppequ")]
+    public static extern int ppequ(LapackLayout Layout, LapackUpLo UpLo,
+                                   int n, double[] ap, double[] s, out double sCond, out double aMax);
+    public static int ppequ(LapackLayout Layout, LapackUpLo UpLo,
+                            int n, double[] ap, out double[] s, out double sCond, out double aMax) {
+      s = new double[n];
+      return ppequ(Layout, UpLo, n, ap, s, out sCond, out aMax);
+    }
   }
 }
