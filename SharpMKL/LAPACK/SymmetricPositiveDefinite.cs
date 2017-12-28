6d5c522036bcc4c1f19bb5c8e3f97934ae7c19fd
Rafka <fw.ge-mu.930@jb4.so-net.ne.jp>
2017-12-28 22:56:13 +0900

    Add porfs functions.

--- SharpMKL/LAPACK/SymmetricPositiveDefinite.cs
+++ SharpMKL/LAPACK/SymmetricPositiveDefinite.cs
@@ -51,8 +51,35 @@ namespace SharpMKLStd {
     [DllImport(LibPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "LAPACKE_spocon")]
     public static extern int pocon(LapackLayout Layout, LapackUpLo UpLo, int n,
                                    float[] a, int lda, float aNorm, out float rCond);
-    [DllImport(LibPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "LAPACKE_dpoequb")]
+    [DllImport(LibPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "LAPACKE_dpocon")]
     public static extern int pocon(LapackLayout Layout, LapackUpLo UpLo, int n,
                                    double[] a, int lda, double aNomr, out double rCond);
+
+    [DllImport(LibPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "LAPACKE_sporfs")]
+    public static extern int porfs(LapackLayout Layout, LapackUpLo UpLo, int n, int nrhs,
+                                   float[] a, int lda, float[] af, int ldaf,
+                                   float[] b, int ldb, float[] x, int ldx,
+                                   float[] fErr, float[] bErr);
+    public static int porfs(LapackLayout Layout, LapackUpLo UpLo, int n, int nrhs,
+                            float[] a, int lda, float[] af, int ldaf,
+                            float[] b, int ldb, float[] x, int ldx,
+                            out float[] fErr, out float[] bErr) {
+      fErr = new float[nrhs > 1 ? nrhs : 1];
+      bErr = new float[nrhs > 1 ? nrhs : 1];
+      return porfs(Layout, UpLo, n, nrhs, a, lda, af, ldaf, b, ldb, x, ldx, fErr, bErr);
+    }
+    [DllImport(LibPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "LAPACKE_dporfs")]
+    public static extern int porfs(LapackLayout Layout, LapackUpLo UpLo, int n, int nrhs,
+                                   double[] a, int lda, double[] af, int ldaf,
+                                   double[] b, int ldb, double[] x, int ldx,
+                                   double[] fErr, double[] bErr);
+    public static int porfs(LapackLayout Layout, LapackUpLo UpLo, int n, int nrhs,
+                            double[] a, int lda, double[] af, int ldaf,
+                            double[] b, int ldb, double[] x, int ldx,
+                            out double[] fErr, out double[] bErr) {
+      fErr = new double[nrhs > 1 ? nrhs : 1];
+      bErr = new double[nrhs > 1 ? nrhs : 1];
+      return porfs(Layout, UpLo, n, nrhs, a, lda, af, ldaf, b, ldb, x, ldx, fErr, bErr);
+    }
   }
 }
