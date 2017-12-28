289434a2e9bf8cfc09a0041fa2c4dc442d22f09d
Rafka <fw.ge-mu.930@jb4.so-net.ne.jp>
2017-12-28 19:27:52 +0900

    Add dttrfb functions.

---
+++ SharpMKL/LAPACK/DiagonallyDominantTridiagonal.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.InteropServices;
+
+namespace SharpMKLStd {
+  public static partial class Lapack {
+    [DllImport(LibPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "sdttrfb")]
+    public static extern void dttrfb(ref int n, float[] dl, float[] d, float[] du, ref int info);
+    public static int dttrfb(int n, float[] dl, float[] d, float[] du) {
+      int info;
+      dttrfb(ref n, dl, du, d, ref info);
+      return info;
+    }
+    [DllImport(LibPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "ddttrfb")]
+    public static extern void dttrfb(ref int n, double[] dl, double[] d, double[] du, ref int info);
+    public static int dttrfb(int n, double[] dl, double[] d, double[] du) {
+      int info;
+      dttrfb(ref n, dl, du, d, ref info);
+      return info;
+    }
+  }
+}
