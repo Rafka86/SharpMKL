17b204c295f5b4f3ebda00bfc6ab0def145ee990
Rafka <fw.ge-mu.930@jb4.so-net.ne.jp>
2017-12-28 13:05:47 +0900

    Add gttrf functions.

---
+++ SharpMKL/LAPACK/GeneralTtridiagonal.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.InteropServices;
+
+namespace SharpMKLStd {
+  public static partial class Lapack {
+    [DllImport(LibPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "LAPACKE_sgttrf")]
+    public static extern int gttrf(int n, float[] dl, float[] d, float[] du, float[] du2, int[] ipiv);
+    public static int gttrf(int n, float[] dl, float[] d, float[] du, out float[] du2, out int[] ipiv) {
+      du2 = new float[n - 2];
+      ipiv = new int[n];
+      return gttrf(n, dl, d, du, du2, ipiv);
+    }
+    [DllImport(LibPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "LAPACKE_dgttrf")]
+    public static extern int gttrf(int n, double[] dl, double[] d, double[] du, double[] du2, int[] ipiv);
+    public static int gttrf(int n, double[] dl, double[] d, double[] du, out double[] du2, out int[] ipiv) {
+      du2 = new double[n - 2];
+      ipiv = new int[n];
+      return gttrf(n, dl, d, du, du2, ipiv);
+    }
+  }
+}
