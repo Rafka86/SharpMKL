92b863c30a6c4b2b02312faa5225711a3afde3c6
Rafka <fw.ge-mu.930@jb4.so-net.ne.jp>
2017-12-28 21:47:31 +0900

    Add potrs functions.

--- SharpMKL/LAPACK/SymmetricPositiveDefinite.cs
+++ SharpMKL/LAPACK/SymmetricPositiveDefinite.cs
@@ -6,5 +6,12 @@ namespace SharpMKLStd {
     public static extern int potrf(LapackLayout Layout, LapackUpLo UpLo, int n, float[] a, int lda);
     [DllImport(LibPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "LAPACKE_dpotrf")]
     public static extern int potrf(LapackLayout Layout, LapackUpLo UpLo, int n, double[] a, int lda);
+    
+    [DllImport(LibPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "LAPACKE_spotrs")]
+    public static extern int potrs(LapackLayout Layout, LapackUpLo UpLo, int n, int nrhs,
+                                   float[] a, int lda, float[] b, int ldb);
+    [DllImport(LibPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "LAPACKE_dpotrs")]
+    public static extern int potrs(LapackLayout Layout, LapackUpLo UpLo, int n, int nrhs,
+                                   double[] a, int lda, double[] b, int ldb);
   }
 }
