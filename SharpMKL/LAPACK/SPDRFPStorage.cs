23217c5da0c28a8d355da1728f47ee902a6b724f
Rafka <fw.ge-mu.930@jb4.so-net.ne.jp>
2017-12-29 12:34:59 +0900

    Add pftrf functions.

---
+++ SharpMKL/LAPACK/SPDRFPStorage.cs
@@ -0,0 +1,12 @@
+﻿//Symmetric Positive Definite RFP Storage
+
+using System.Runtime.InteropServices;
+
+namespace SharpMKLStd {
+  public static partial class Lapack {
+    [DllImport(LibPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "LAPACKE_spftrf")]
+    public static extern int pftrf(LapackLayout Layout, LapackTranspose Trans, LapackUpLo UpLo, int n, float[] a);
+    [DllImport(LibPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "LAPACKE_dpftrf")]
+    public static extern int pftrf(LapackLayout Layout, LapackTranspose Trans, LapackUpLo UpLo, int n, double[] a);
+  }
+}
