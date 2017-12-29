9c30640434824ca19b7f5d658a002df0c9313292
Rafka <fw.ge-mu.930@jb4.so-net.ne.jp>
2017-12-29 12:52:35 +0900

    Add pbtrf functions.

---
+++ SharpMKL/LAPACK/SymmetricPositiveDefiniteBand.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices;
+
+namespace SharpMKLStd {
+  public static partial class Lapack {
+    [DllImport(LibPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "LAPACKE_spbtrf")]
+    public static extern int pbtrf(LapackLayout Layout, LapackUpLo UpLo, int n, int kd, float[] ab, int ldab);
+    [DllImport(LibPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "LAPACKE_dpbtrf")]
+    public static extern int pbtrf(LapackLayout Layout, LapackUpLo UpLo, int n, int kd, double[] ab, int ldab);
+    
+  }
+}
