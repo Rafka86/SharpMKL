2601bde37cc73a91452f92bd407da8840c99f623
Rafka <fw.ge-mu.930@jb4.so-net.ne.jp>
2017-12-29 12:56:24 +0900

    Add pbtrs functions.

--- SharpMKL/LAPACK/SPDRFPStorage.cs
+++ SharpMKL/LAPACK/SPDRFPStorage.cs
@@ -10,9 +10,11 @@ namespace SharpMKLStd {
     public static extern int pftrf(LapackLayout Layout, LapackTranspose Trans, LapackUpLo UpLo, int n, double[] a);
     
     [DllImport(LibPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "LAPACKE_spftrs")]
-    public static extern int pftrs(LapackLayout Layout, LapackTranspose Trans, LapackUpLo UpLo, int n, int nrhs, float[] a, float[] b, int ldb);
+    public static extern int pftrs(LapackLayout Layout, LapackTranspose Trans, LapackUpLo UpLo,
+                                   int n, int nrhs, float[] a, float[] b, int ldb);
     [DllImport(LibPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "LAPACKE_dpftrs")]
-    public static extern int pftrs(LapackLayout Layout, LapackTranspose Trans, LapackUpLo UpLo, int n, int nrhs, double[] a, double[] b, int ldb);
+    public static extern int pftrs(LapackLayout Layout, LapackTranspose Trans, LapackUpLo UpLo,
+                                   int n, int nrhs, double[] a, double[] b, int ldb);
     
     [DllImport(LibPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "LAPACKE_spftri")]
     public static extern int pftri(LapackLayout Layout, LapackTranspose Trans, LapackUpLo UpLo, int n, float[] a);
--- SharpMKL/LAPACK/SymmetricPositiveDefiniteBand.cs
+++ SharpMKL/LAPACK/SymmetricPositiveDefiniteBand.cs
@@ -7,5 +7,9 @@ namespace SharpMKLStd {
     [DllImport(LibPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "LAPACKE_dpbtrf")]
     public static extern int pbtrf(LapackLayout Layout, LapackUpLo UpLo, int n, int kd, double[] ab, int ldab);
     
+    [DllImport(LibPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "LAPACKE_spbtrs")]
+    public static extern int pbtrs(LapackLayout Layout, LapackUpLo UpLo, int n, int kd, int nrhs, float[] ab, int ldab, float[] b, int ldb);
+    [DllImport(LibPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "LAPACKE_dpbtrs")]
+    public static extern int pbtrs(LapackLayout Layout, LapackUpLo UpLo, int n, int kd, int nrhs, double[] ab, int ldab, double[] b, int ldb);
   }
 }
